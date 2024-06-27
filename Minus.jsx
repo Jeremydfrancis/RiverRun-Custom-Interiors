896047fab0bdd4edbf69905ee22faae1181d6110
Jeremydfrancis <126933820+Jeremydfrancis@users.noreply.github.com>
2024-06-27 13:28:08 -0400

    Assets for the Website
    Assets include svgs converted into JSX

---
+++ Minus.jsx
@@ -0,0 +1,4 @@
+const Minus = () => (
+      <svg id="Layer_2_00000109010831470149334250000013452754770502125465_" enableBackground="new 0 0 512 512" height="30" viewBox="0 0 512 512" width="30" xmlns="http://www.w3.org/2000/svg"><g id="Icon"><path id="_40" d="m401 231v50h-290v-50z"/></g></svg>
+    );
+    export default Minus;
